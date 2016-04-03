e217ba271d7447c09d690d9129ecdf04d15a5bb6
Terrence Ezrol <ezterry3@gmail.com>
2016-04-03 13:39:44 -0400

    make object read only (since defaults will not change)

--- src/main/java/com/ezrol/terry/minecraft/biomeheighttweaker/BiomeHeightData.java
+++ src/main/java/com/ezrol/terry/minecraft/biomeheighttweaker/BiomeHeightData.java
@@ -2,17 +2,11 @@ package com.ezrol.terry.minecraft.biomeheighttweaker;
 
 public class BiomeHeightData {
 	/**
-	 * store the height and variation of a particular biome
+	 * store the default height and variation of a particular biome
 	 */
 	private float height;
 	private float variation;
 	private String name;
-
-	public BiomeHeightData(String name) {
-		this.name = name;
-		this.height = 0.0f;
-		this.variation = 0.0f;
-	}
 
 	public BiomeHeightData(String name, float height, float variation) {
 		this.name = name;
@@ -26,19 +20,6 @@ public class BiomeHeightData {
 
 	public float getVariation() {
 		return variation;
-	}
-
-	public void update(float height, float variation) {
-		this.height = height;
-		this.variation = variation;
-	}
-
-	public void setHeight(float height) {
-		this.height = height;
-	}
-
-	public void setVariation(float variation) {
-		this.variation = variation;
 	}
 
 	public String getName() {
